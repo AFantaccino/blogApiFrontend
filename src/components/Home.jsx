345c11cd5ac121183d273915ab83a0ef89998cc7
Alessandro F <alessandro.fantaccino@gmail.com>
2023-10-19 11:10:45 +0200

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-	return <div>test</div>;
+	return (
+		<div className="flex justify-center">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+			veritatis itaque sequi. Quod, maxime tempore?
+		</div>
+	);
 };
 
 export default Home;
